74d73ad91fcb4e8174b2492320f2748a75d1aebc
osyshyn <ceo@comebackagency.com>
2026-01-21 19:33:24 +0100

    update

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -9,6 +9,14 @@ const manrope = Manrope({ subsets: ["latin"], variable: "--font-manrope" });
 export const metadata: Metadata = {
   title: "EdgeBet Coming soon",
   description: "EdgeBet Coming soon",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-48.png", sizes: "48x48", type: "image/png" },
+    ],
+  },
 };
 
 export default function RootLayout({
